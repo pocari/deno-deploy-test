b25041c7b471a1dd20018d7d23c63146aa852295
pocari <caffelattenonsugar@gmail.com>
2021-09-07 11:02:10 +0900

    静的なファイルテスト

--- hello.ts
+++ hello.ts
@@ -1,8 +1,31 @@
-addEventListener("fetch", (event) => {
+addEventListener("fetch", async (event) => {
   const { pathname } = new URL(event.request.url);
   if (pathname == '/') {
-    event.respondWith(new Response("Hello world xx"));
-  } else if (pathname == '/hoge') {
-    event.respondWith(new Response("Hello world yy"));
+    event.respondWith(new Response(
+      new TextDecoder().decode(await Deno.readFile("./index.html")),
+      {
+        headers: {
+          'content-type': 'text/html'
+        }
+      }
+    ));
+  } else if (pathname == '/json') {
+    event.respondWith(new Response(
+      JSON.stringify({ name: "deno", app: "deploy"}),
+      {
+        headers: {
+          'content-type': 'application/json'
+        }
+      }
+    ));
+  } else if (pathname == '/image/x') {
+    event.respondWith(new Response(
+      await Deno.readFile("./deno_logo.svg"),
+      {
+        headers: {
+          'content-type': 'image/svg+xml'
+        }
+      }
+    ));
   }
 });
