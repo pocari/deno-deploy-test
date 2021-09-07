3b7dc173f81a1c19dd149c5507141cde40c5889d
pocari <caffelattenonsugar@gmail.com>
2021-09-07 10:48:23 +0900

    mod1

--- hello.ts
+++ hello.ts
@@ -1,3 +1,8 @@
 addEventListener("fetch", (event) => {
-  event.respondWith(new Response("Hello world"));
+  const { path } = new URL(event.request.url);
+  if (pathname == '/') {
+    event.respondWith(new Response("Hello world xx"));
+  } else if (pathname == '/hoge') {
+    event.respondWith(new Response("Hello world yy"));
+  }
 });
